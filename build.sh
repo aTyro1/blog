78c74b26846eab46d7df1810ade9af89568adad4
aTyro1 <2022amankumar@gmail.com>
2024-01-25 15:13:13 +0530

    updating jenkinfile

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python3 -m venv new
-source new/bin/activate
-pip install -r requirements.txt
+cd 
+source django/bin/activate
+cd cdac_django
 python3 manage.py test blogs
 python3 manage.py test writers
